beb11450ca3114191bc4b2b67dddb71188c902e7
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-12 14:24:25 +0100

    feat: create server side with init config

---
+++ server/Controllers/LandOfFourPawsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTalkForKids_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandOfFourPawsController : ControllerBase
+    {
+    }
+}
---
+++ server/Controllers/LandOfSunnyDreamsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTalkForKids_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandOfSunnyDreamsController : ControllerBase
+    {
+    }
+}
