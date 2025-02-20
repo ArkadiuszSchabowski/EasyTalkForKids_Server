5d92c58902b6935bf2158eb74e3a940ce3804814
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-21 00:45:38 +0100

    feat: add error handling middleware and own exceptions

---
+++ server/Exceptions/ConflictException.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyTalkForKids.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) 
+        {
+        
+        }
+    }
+}
---
+++ server/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyTalkForKids.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        { 
+        
+        }
+    }
+}
