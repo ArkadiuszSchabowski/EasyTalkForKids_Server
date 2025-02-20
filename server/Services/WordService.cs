6f4869ed19dec61c978b406d324f3ea956215f03
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-21 00:50:21 +0100

    feat: update Add method

--- server/Services/WordService.cs
+++ server/Services/WordService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using EasyTalkForKids.Exceptions;
 using EasyTalkForKids.Interfaces;
 using EasyTalkForKids.Models;
 using EasyTalkForKids_Database.Entities;
@@ -9,17 +10,28 @@ namespace EasyTalkForKids.Services
     public class WordService : IService<WordDto>
     {
         private readonly IRepository<Word> _repository;
+        private readonly IRepository<Lesson> _lessonRepository;
         private readonly IMapper _mapper;
 
-        public WordService(IRepository<Word> repository, IMapper mapper)
+        public WordService(IRepository<Word> repository, IRepository<Lesson> lessonRepository,IMapper mapper)
         {
             _repository = repository;
+            _lessonRepository = lessonRepository;
             _mapper = mapper;
         }
 
         public void Add(WordDto dto)
         {
+            var lesson = _lessonRepository.Get(dto.LessonId);
+
+            if(lesson == null)
+            {
+                throw new NotFoundException("Nie znaleziono lekcji o takim numerze Id");
+            }
+
             var word = _mapper.Map<Word>(dto);
+
+            _repository.Add(word);
         }
 
         public List<WordDto> Get()
