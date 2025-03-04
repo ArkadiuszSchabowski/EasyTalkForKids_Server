cf36595d50bc1c100d52f5c3ddeb2ccbfd612f5f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-04 12:44:03 +0100

    feat: create category endpoints

--- server/Controllers/CategoryController.cs
+++ server/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyTalkForKids.Interfaces;
+using EasyTalkForKids.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyTalkForKids.Controllers
 {
@@ -6,5 +8,48 @@ namespace EasyTalkForKids.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly IAddService<AddCategoryDto> _addService;
+        private readonly IGetService<GetCategoryDto> _getService;
+        private readonly IRemoveService<RemoveCategoryDto> _removeService;
+
+        public CategoryController(IAddService<AddCategoryDto> addService, IGetService<GetCategoryDto> getService, IRemoveService<RemoveCategoryDto> removeService)
+        {
+            _addService = addService;
+            _getService = getService;
+            _removeService = removeService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<GetCategoryDto>> Get()
+        {
+            List<GetCategoryDto> dto = _getService.Get();
+
+            return dto;
+        }
+
+
+        [HttpPost]
+        public ActionResult Add([FromBody] AddCategoryDto dto)
+        {
+            _addService.Add(dto);
+
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AddCategoryDto> Get([FromRoute] int id)
+        {
+            var dto = _getService.Get(id);
+
+            return Ok(dto);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remove([FromRoute] int id)
+        {
+            _removeService.Remove(id);
+
+            return NoContent();
+        }
     }
 }
---
+++ server/Services/CategoryService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using EasyTalkForKids.Exceptions;
+using EasyTalkForKids.Interfaces;
+using EasyTalkForKids.Models;
+using EasyTalkForKids_Database.Entities;
+
+namespace EasyTalkForKids.Services
+{
+    public class CategoryService : IAddService<AddCategoryDto>, IGetService<GetCategoryDto>, IRemoveService<RemoveCategoryDto>
+    {
+        private readonly IRepository<Category> _repository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IRepository<Category> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void Add(AddCategoryDto dto)
+        {
+            var category = _mapper.Map<Category>(dto);
+
+            _repository.Add(category);
+        }
+
+        public List<GetCategoryDto> Get()
+        {
+            List<Category> categories = _repository.Get();
+
+            var dto = _mapper.Map<List<GetCategoryDto>>(categories);
+
+            return dto;
+        }
+
+        public GetCategoryDto Get(int id)
+        {
+            Category? category = _repository.Get(id);
+
+            if (category == null)
+            {
+                throw new NotFoundException("Nie znaleziono kategorii o takim numerze Id!");
+            }
+
+            var dto = _mapper.Map<GetCategoryDto>(category);
+
+            return dto;
+        }
+
+        public void Remove(int id)
+        {
+            Category? category = _repository.Get(id);
+
+            if (category == null)
+            {
+                throw new NotFoundException("Nie znaleziono kategorii o takim numerze Id!");
+            }
+
+            _repository.Remove(category);
+        }
+    }
+}
