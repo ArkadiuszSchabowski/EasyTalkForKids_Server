c8e5a97d257ad6150c21897b300fc974e331b035
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-16 15:27:46 +0100

    fix: update AddCategory, AddLesson and AddWord dto's

--- server/Models/AddLessonDto.cs
+++ server/Models/AddLessonDto.cs
@@ -4,6 +4,5 @@
     {
         public int TopicId { get; set; }
         public int CategoryId { get; set; }
-        public List<AddWordDto> WordsDto { get; set; } = new List<AddWordDto>();
     }
 }
