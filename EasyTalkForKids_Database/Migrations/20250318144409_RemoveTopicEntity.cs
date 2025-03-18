be331a7d8adf9615ab4ad4fa2dbacd8e75f6b977
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-18 15:44:38 +0100

    migration: remove topic entity

---
+++ EasyTalkForKids_Database/Migrations/20250318144409_RemoveTopicEntity.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyTalkForKids_Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTopicEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Topics_TopicId",
+                table: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_TopicId",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "TopicId",
+                table: "Lessons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishName",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PolishName",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnglishName",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "PolishName",
+                table: "Lessons");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TopicId",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnglishName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PolishName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_TopicId",
+                table: "Lessons",
+                column: "TopicId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Topics_TopicId",
+                table: "Lessons",
+                column: "TopicId",
+                principalTable: "Topics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
