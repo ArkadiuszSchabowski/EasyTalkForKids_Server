458b36a44cda50ede7d42ca3a473806f83fc45f0
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-26 12:40:02 +0100

    feat: create validators

---
+++ server/Validators/CategoryValidator.cs
@@ -0,0 +1,34 @@
+﻿using EasyTalkForKids.Exceptions;
+using EasyTalkForKids.Interfaces;
+
+namespace EasyTalkForKids.Validators
+{
+    public class CategoryValidator : ICategoryValidator
+    {
+        private readonly IRepositoryCategory _repositoryCategory;
+
+        public CategoryValidator(IRepositoryCategory repositoryCategory)
+        {
+            _repositoryCategory = repositoryCategory;
+        }
+        public void ThrowIfPolishNameExists(string name)
+        {
+            var category = _repositoryCategory.GetByPolishName(name);
+
+            if (category != null)
+            {
+                throw new ConflictException("Polska nazwa kategorii isnieje już w bazie danych!");
+            }
+        }
+
+        public void ThrowIfEnglishNameExists(string name)
+        {
+            var category = _repositoryCategory.GetByEnglishName(name);
+
+            if (category != null)
+            {
+                throw new ConflictException("Angielska nazwa kategorii isnieje już w bazie danych!");
+            }
+        }
+    }
+}
