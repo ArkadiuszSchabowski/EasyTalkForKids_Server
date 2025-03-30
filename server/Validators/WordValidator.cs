573390fd6500ec542919c4cf9fcd50ab40a9a0d3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-30 20:00:13 +0200

    feat: create IWordValidator interface and WordValidator class

---
+++ server/Validators/WordValidator.cs
@@ -0,0 +1,17 @@
+﻿using EasyTalkForKids.Exceptions;
+using EasyTalkForKids.Interfaces;
+using EasyTalkForKids_Database.Entities;
+
+namespace EasyTalkForKids.Validators
+{
+    public class WordValidator : IWordValidator
+    {
+        public void ThrowIfWordIsNull(Word? word)
+        {
+            if (word == null)
+            {
+                throw new NotFoundException("Nie znaleziono słowa o takim numerze Id!");
+            }
+        }
+    }
+}
