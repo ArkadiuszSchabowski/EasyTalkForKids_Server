91c6c42fcf9a0a84d139d97fb100e8c1bbbf01ad
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-30 12:50:42 +0200

    chore: register ILessonValidator with LessonValidator in DI container

--- server/Program.cs
+++ server/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddScoped<IRepository<Word>, WordRepository>();
 builder.Services.AddScoped<IRepositoryCategory, CategoryRepository>();
 
 builder.Services.AddScoped<ICategoryValidator, CategoryValidator>();
+builder.Services.AddScoped<ILessonValidator, LessonValidator>();
 builder.Services.AddScoped<INameValidator, NameValidator>();
 
 builder.Services.AddCors(options =>
