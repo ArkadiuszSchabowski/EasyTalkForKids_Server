fa9904b2fa7c56c8c0900159a1624b72ffaaf5d3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-04-12 20:58:54 +0200

    feat: update Add method

--- server/Services/WordService.cs
+++ server/Services/WordService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using EasyTalkForKids.Interfaces;
 using EasyTalkForKids.Models;
+using EasyTalkForKids.Validators;
 using EasyTalkForKids_Database.Entities;
 
 namespace EasyTalkForKids.Services
@@ -12,22 +13,33 @@ namespace EasyTalkForKids.Services
         private readonly IRepository<Lesson> _lessonRepository;
         private readonly IWordValidator _wordValidator;
         private readonly ILessonValidator _lessonValidator;
+        private readonly INameValidator _nameValidator;
         private readonly IMapper _mapper;
 
-        public WordService(IRepository<Word> wordRepository, IRepository<Lesson> lessonRepository, IWordValidator wordValidator, ILessonValidator lessonValidator, IMapper mapper)
+        public WordService(IRepository<Word> wordRepository, IRepository<Lesson> lessonRepository, IWordValidator wordValidator, ILessonValidator lessonValidator, INameValidator nameValidator, IMapper mapper)
         {
             _wordRepository = wordRepository;
             _lessonRepository = lessonRepository;
             _wordValidator = wordValidator;
             _lessonValidator = lessonValidator;
+            _nameValidator = nameValidator;
             _mapper = mapper;
         }
 
         public void Add(AddWordDto dto)
         {
+            _wordValidator.ThrowIfPolishNameIsNull(dto.PolishName);
+            _wordValidator.ThrowIfEnglishNameIsNull(dto.EnglishName);
+
+            _nameValidator.ValidateName(dto.PolishName);
+            _nameValidator.ValidateName(dto.EnglishName);
+
             var lesson = _lessonRepository.Get(dto.LessonId);
 
             _lessonValidator.ThrowIfLessonIsNull(lesson);
+
+            dto.PolishName = dto.PolishName.ToLower();
+            dto.EnglishName = dto.EnglishName.ToLower();
 
             var word = _mapper.Map<Word>(dto);
 
