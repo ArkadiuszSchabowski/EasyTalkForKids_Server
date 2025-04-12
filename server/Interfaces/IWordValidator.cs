5c41370f7dffbf0adc0da64940ea698a727ab732
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-04-12 20:56:43 +0200

    feat: add methods for ThrowIfEnglishNameIsNull and ThrowIfPolishNameIsNull

--- server/Interfaces/IWordValidator.cs
+++ server/Interfaces/IWordValidator.cs
@@ -4,6 +4,8 @@ namespace EasyTalkForKids.Interfaces
 {
     public interface IWordValidator
     {
+        public void ThrowIfEnglishNameIsNull(string name);
+        public void ThrowIfPolishNameIsNull(string name);
         public void ThrowIfWordIsNull(Word? word);
     }
 }
